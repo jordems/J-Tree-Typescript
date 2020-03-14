d0aef01cc08808782b73e5da42dd821523cfaa52
Jordan Emslie <jordems@gmail.com>
2020-03-14 13:20:54 -0700

    Setup Junction Tree Strucuture

--- src/JunctionTree.ts
+++ src/JunctionTree.ts
@@ -1,5 +1,16 @@
 import BayesianNetwork from "./BayesianNetwork";
 
 export default class JunctionTree {
-  constructor(bayesnet: BayesianNetwork) {}
+  private bnet: BayesianNetwork;
+  constructor(bnet: BayesianNetwork) {
+    this.bnet = bnet;
+  }
+
+  private transformfromBayesNetwork(): void {}
+
+  private initialize(): void {}
+
+  private propogate(): void {}
+
+  private marginalize(): void {}
 }
